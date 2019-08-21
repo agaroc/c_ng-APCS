4edef9b5d8079c99ca3c89b5dfbad350d9c2bf19
APCS2nd <APCS2nd@MHS-LT9-14>
2019-08-21 12:17:15 -0700

    Complete Hello World Project

---
+++ HelloWorld/src/Hello.java
@@ -0,0 +1,10 @@
+/*Caleb Ng 
+ * 8/16/19
+ * First project for Java in AP CS
+ */
+public class Hello {
+	public static void main(String[] args) {
+		System.out.println("HelloWorld");  
+		System.out.println("HelloWorld");
+	}
+}
