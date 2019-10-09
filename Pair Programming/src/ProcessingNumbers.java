832e2415dc27715aaf408c541754e2513099cf42
agaroc <44041425+agaroc@users.noreply.github.com>
2019-10-08 21:09:19 -0700

    Finishing touches

--- Pair Programming/src/ProcessingNumbers.java
+++ Pair Programming/src/ProcessingNumbers.java
@@ -1,35 +1,43 @@
 /*Caleb Ng
  * 10/6/19
- * Ask for numbers and returns the evben numbers, min , max, and max of the even numbers
+ * Ask for numbers and returns the sum of even numbers, min , max, and max of the even numbers
  */
 import java.util.*;
 public class ProcessingNumbers {
 	public static void main(String[]args) {
+		int min = 2147483647;
+		int max= -2147483648;
 		Scanner userInput = new Scanner(System.in);
-		int max = 0;
 		int currentNumber = 0;
-		String evenNum = "";
+		int sumEven = 0;
 		int maxEven = 0;
 		System.out.print("How many numbers do you have? ");
 		int times = userInput.nextInt();
-		System.out.print("Whats your Number?");
-		int min = userInput.nextInt();
-		for(var i = 1; i < times; i ++) {
-			System.out.print("Whats your Number?");
-			currentNumber = userInput.nextInt();
-			if(currentNumber % 2 ==0) {
-				evenNum += (currentNumber + ",");
-				if(maxEven < currentNumber) {
-					maxEven = currentNumber; 
+		if(times > 0) {
+			for(var i = 1; i <= times; i ++) {
+				System.out.print("Whats your Number?");
+				currentNumber = userInput.nextInt();
+				if(currentNumber % 2 == 0) {
+					sumEven += currentNumber;
+					if(max < currentNumber) {
+						maxEven = currentNumber; 
+					}
 				}
+				if(max < currentNumber) {
+					max = currentNumber;
+				}
+				if(min > currentNumber){
+					min = currentNumber;
+				}			
 			}
-			if(max < currentNumber) {
-				max = currentNumber;
-			}else if(min > currentNumber){
-				min = currentNumber;
-			}			
+		}else {
+			throw new IllegalArgumentException("You can't have a negative amount of numbers");
 		}
-		System.out.println("Max: " + max + "\nMin: " +  min + "\nEvens: " + evenNum + "\nMaxEven: " + maxEven);
+		if(maxEven == 0) {
+			System.out.println("Max: " + max + "\nMin: " +  min + "\nSumeven: " + sumEven + "\nMaxEven: No even number" );
+		}else {
+			System.out.println("Max: " + max + "\nMin: " +  min + "\nSumeven: " + sumEven + "\nMaxEven: " + maxEven);
+		}
 		userInput.close();
 	}
 }
