ccb0c9bfc2578b338b3f27f935053d8fccdfcf0c
APCS2nd <APCS2nd@MHS-LT9-14>
2019-11-01 11:26:08 -0700

    Almost done

--- FracCalc/src/fracCalc/FracCalc.java
+++ FracCalc/src/fracCalc/FracCalc.java
@@ -1,11 +1,17 @@
 package fracCalc;
-
+import java.util.*;
 public class FracCalc {
 
-    public static void main(String[] args) 
-    {
-        // TODO: Read the input from the user and call produceAnswer with an equation
-
+    public static void main(String[] args) {
+    	Scanner userInput = new Scanner(System.in);
+    	String stop = "";
+    	do {
+    		System.out.println("Enter your fractions:");
+    		String input = userInput.nextLine();
+    		System.out.println(produceAnswer(input));
+    		System.out.println("Do you want to continue? Type (quit) to exit");
+    		stop = userInput.nextLine();
+    	}while(!stop.equals("quit"));	
     }
     
     // ** IMPORTANT ** DO NOT DELETE THIS FUNCTION.  This function will be used to test your code
@@ -16,12 +22,18 @@ public class FracCalc {
     //        
     // The function should return the result of the fraction after it has been calculated
     //      e.g. return ==> "1_1/4"
-    public static String produceAnswer(String input)
-    { 
+    public static String produceAnswer(String input){
+    	String[] splitOpperands = input.split(" ");
+    	String opperand1 = splitOpperands[0];
+    	String opperand2 = splitOpperands[1];
+    	String opperand3 = splitOpperands[2];
+    	return opperand3;
+    }
+     
         // TODO: Implement this function to produce the solution to the input
         
-        return "";
-    }
+        
+  
 
     // TODO: Fill in the space below with any helper methods that you think you will need
     
